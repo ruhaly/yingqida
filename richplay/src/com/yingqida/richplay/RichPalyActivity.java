6b4f8d10fbd938285738f7edbef7f9afa299ad5f
han_lru <han_lru@163.com>
2013-08-26 18:43:53 +0800

    Merge branch 'master' of https://github.com/ruhaly/yingqida
    Conflicts:
    	richplay/src/com/paintpad/activity/Main.java
    	richplay/src/com/paintpad/view/PaintView.java

--- richplay/src/com/yingqida/richplay/RichPalyActivity.java
+++ richplay/src/com/yingqida/richplay/RichPalyActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.zxing.client.android.CaptureActivity;
 import com.yingqida.richplay.activity.LoginActivity;
 import com.yingqida.richplay.activity.common.SuperActivity;
 import com.yingqida.richplay.baseapi.AppUtil;
@@ -71,7 +72,7 @@ public class RichPalyActivity extends SuperActivity {
 							HomeActivity.class));
 				} else {
 					startActivity(new Intent(getBaseContext(),
-							LoginActivity.class));
+							CaptureActivity.class));
 				}
 				finish();
 			}
